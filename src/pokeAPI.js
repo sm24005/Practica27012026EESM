eb830c5fd74da9b6317527b47deea8587b23d23a
enrique_sm117 <enrique_sm117@outlook.es>
2026-02-03 22:54:08 -0600

    .

--- src/pokeAPI.js
+++ src/pokeAPI.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+const url = import.meta.env.VITE_API_URLPOKE;
 const api = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2',
+ // baseURL: 'https://pokeapi.co/api/v2',
+  baseURL: url,
   timeout: 5000,
   headers: {'Content-Type': 'application/json'}
 });
